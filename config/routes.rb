55b37f19a971d0d115ffa82a447aadbaaa990d32
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-09 18:25:26 +0800

    adjusted arrangement of lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :meetings
-  resources :messages
   devise_for :users
   resources :organisations
   resources :teams
+  resources :meetings
+  resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   namespace :api do
