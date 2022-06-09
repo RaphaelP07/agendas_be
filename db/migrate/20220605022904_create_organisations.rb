8d7e6fd8599cd6663f5575b0b3f8fb80aa71aca0
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-09 17:32:58 +0800

    changed column name to city address

--- db/migrate/20220605022904_create_organisations.rb
+++ db/migrate/20220605022904_create_organisations.rb
@@ -2,7 +2,7 @@ class CreateOrganisations < ActiveRecord::Migration[7.0]
   def change
     create_table :organisations do |t|
       t.string :name, null: false
-      t.string :address
+      t.string :city_address
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
