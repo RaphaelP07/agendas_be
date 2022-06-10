9ef96268c2cb996919065430f420d1fafbf5190e
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-10 14:53:33 +0800

    added postgres and timezone

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module AgendasBe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = "Asia/Shanghai"
+    config.active_record.default_timezone = :local
   end
 end
