34f55bf02be5ab36f66b48a6e3c007db1adcf24e
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-11 16:16:43 +0800

    added namespace to controller

--- app/controllers/api/v1/organisations_controller.rb
+++ app/controllers/api/v1/organisations_controller.rb
@@ -1,63 +1,71 @@
-class OrganisationsController < ApplicationController
-  before_action :set_organisation, only: %i[ show update destroy create_team ]
+module Api
+  module V1
+    class OrganisationsController < ApplicationController
+      before_action :set_organisation, only: %i[ show update destroy create_team ]
 
-  # GET /organisations
-  def index
-    @organisations = Organisation.all
+      # GET /organisations
+      def index
+        @organisations = Organisation.all
 
-    render json: @organisations
-  end
+        render json: @organisations
+      end
 
-  # GET /organisations/1
-  def show
-    render json: @organisation
-  end
+      # GET /organisations/1
+      def show
+        render json: @organisation
+      end
 
-  # POST /organisations
-  def create
-    @organisation = Organisation.new(organisation_params)
+      # POST /organisations
+      def create
+        @organisation = Organisation.new(organisation_params)
 
-    if @organisation.save
-      render json: @organisation, status: :created, location: @organisation
-    else
-      render json: @organisation.errors, status: :unprocessable_entity
+        if @organisation.save
+          render json: @organisation, status: :created, location: @organisation
+        else
+          render json: @organisation.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /organisations/1
+      def update
+        if @organisation.update(organisation_params)
+          render json: @organisation
+        else
+          render json: @organisation.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /organisations/1
+      def destroy
+        @organisation.destroy
+      end
+
+      # POST /organisations/1/teams
+      def create_team
+        @organisation
+      end
+
+      private
+
+      def set_organisation
+        @organisation = Organisation.find(params[:id])
+      end
+
+      # def get_user
+      #   @user = 
+      # end
+
+      def organisation_params
+        params.require(:organisation).permit(:name, :city_address)
+      end
+
+      def team_params
+        params.require(:team).permit(:name, :organisation_id)
+      end
+
+      def meeting_params
+        params.require(:meeting).permit(:agenda, :notes)
+      end
     end
   end
-
-  # PATCH/PUT /organisations/1
-  def update
-    if @organisation.update(organisation_params)
-      render json: @organisation
-    else
-      render json: @organisation.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /organisations/1
-  def destroy
-    @organisation.destroy
-  end
-
-  # POST /organisations/1/teams
-  def create_team
-    @organisation
-  end
-
-  private
-
-    def set_organisation
-      @organisation = Organisation.find(params[:id])
-    end
-
-    def organisation_params
-      params.require(:organisation).permit(:name, :city_address)
-    end
-
-    def team_params
-      params.require(:team).permit(:name, :organisation_id)
-    end
-
-    def meeting_params
-      params.require(:meeting).permit(:agenda, :notes)
-    end
 end
