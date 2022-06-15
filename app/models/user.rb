01f62174648ba195ea538bb954567cbe23eed459
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-15 16:58:51 +0800

    added jti

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, 
-         :omniauthable, omniauth_providers: [:google_oauth2], 
+         :omniauthable, omniauth_providers: [:google_oauth2],
          :jwt_authenticable, jwt_recovation_strategy: self
 
   has_and_belongs_to_many :organisations
