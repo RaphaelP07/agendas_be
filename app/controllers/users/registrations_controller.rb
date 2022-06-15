8e10d4be69a4867e77331e70039ef562ef45251a
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-15 20:21:22 +0800

    fix sessions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionFix
   respond_to :json
+
   private
 
   def respond_with(resource, _opts = {})
