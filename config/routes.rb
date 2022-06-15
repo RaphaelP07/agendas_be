cc46e10fc3efded3068d40ede488360b2fdee903
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-15 20:22:41 +0800

    add route for current user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
+      get '/current_user', to: 'current_user#index'
       devise_for :users, path: '', path_names: {
         sign_in: 'login',
         sign_out: 'logout',
