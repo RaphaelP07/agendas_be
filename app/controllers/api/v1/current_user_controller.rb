35b67728e96c5deafc2de6cd63984c7f843b2598
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-16 16:06:20 +0800

    fixed current_user controller

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -3,7 +3,7 @@ module Api
     class CurrentUserController < ApplicationController
       before_action :authenticate_api_v1_user!
       def index
-        render json: current_user, status: :ok
+        render json: current_api_v1_user, status: :ok
       end
     end 
   end
