f728c967b881c9b8c07b9c1c382cf008bf1c3b9e
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-20 20:41:31 +0800

    fixed member validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_api_v1_user!
+  before_action :member_is_not_in_org
   
   private
 
@@ -13,5 +14,19 @@ class ApplicationController < ActionController::API
 
   def not_admin
     not_admin = @organisation.admin != current_api_v1_user
+  end
+
+  def member_is_not_in_org
+    member_is_not_in_org = Organisation.find(
+      params[:id] == nil ? return :
+      params[:organisation_id] == nil ? params[:id] : 
+      params[:organisation_id]
+      ).users.exists?(current_api_v1_user['id']) == false
+    
+    if member_is_not_in_org
+      render json: {
+        message: 'You are not part of this organisation.'
+      }, status: :method_not_allowed
+    end
   end
 end
