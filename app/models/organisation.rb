55a10ccf1b246a6c15234a9d6f5b53ba07f90586
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-23 23:12:09 +0800

    made teams and meetings dependent to destruction of org

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,7 +1,13 @@
 class Organisation < ApplicationRecord
   has_and_belongs_to_many :users
-  has_many :teams
-  has_many :meetings
+  has_many(
+    :teams,
+    dependent: :destroy
+  )
+  has_many(
+    :meetings,
+    dependent: :destroy
+  )
   belongs_to :admin, class_name: 'User', foreign_key: 'admin_id'
 
   validates :name, uniqueness: true
