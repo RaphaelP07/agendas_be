2c9ea76a5a971fb5c183886b4fada5e697875396
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-30 15:25:58 +0800

    changed pathname column from string to text

--- db/migrate/20220630071731_add_pathname_to_video.rb
+++ db/migrate/20220630071731_add_pathname_to_video.rb
@@ -1,5 +1,5 @@
 class AddPathnameToVideo < ActiveRecord::Migration[7.0]
   def change
-    add_column :videos, :pathname, :string
+    add_column :videos, :pathname, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -113,7 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_071731) do
     t.bigint "meeting_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "pathname"
+    t.text "pathname"
     t.index ["meeting_id"], name: "index_videos_on_meeting_id"
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
